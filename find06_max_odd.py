dfe716b318be20a1a9497733a7eeccc85cd9f4b9
allamurodxakimov <xakimov2806@gmail.com>
2023-10-07 18:37:10 -0700

    find max 6

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -1,8 +1,21 @@
-def find_max_odd(data):
+def find_max_odd(lst):
     """
     Given the list of numbers, Find the maximum odd number in the list
     args:
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    a=0
+    ls=[]
+    while a<len(lst):
+        if lst[a]%2==1:
+            ls.append(lst[a])
+        a+=1
+    c=0
+    mi=ls[0]
+    while c<len(ls):
+        if ls[c]>=mi:
+            mi=ls[c]
+        c+=1
+    return mi
+print(find_max_odd([1,2,32,4,5,7]))
