debee2e375a937b4cc157caf0d324ebc1cbbde7f
allamurodxakimov <xakimov2806@gmail.com>
2023-10-07 18:38:11 -0700

    find min 8

--- find08_min_count.py
+++ find08_min_count.py
@@ -5,4 +5,12 @@ def find_min_count(data):
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    a=0
+    mi=data[0]
+    while a<len(data):
+        if mi>=data[a]:
+            mi=data[a]
+        a+=1
+    k=data.count(mi)
+    return k
+print(find_min_count([2,3,4,-4,3,-4,-4]))
